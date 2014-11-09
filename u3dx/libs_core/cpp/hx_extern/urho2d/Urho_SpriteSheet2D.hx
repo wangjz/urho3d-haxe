911923ed99c43808b66b353aedf655b85be2775f
wangjz <421924396@qq.com>
2014-11-09 16:19:19 +0800

    continue
    continue

--- u3dx/libs_core/cpp/hx_extern/urho2d/Urho_SpriteSheet2D.hx
+++ u3dx/libs_core/cpp/hx_extern/urho2d/Urho_SpriteSheet2D.hx
@@ -1,5 +1,12 @@
 package u3dx.libs_core.cpp.hx_extern.urho2d;
+import cpp.Char;
+import cpp.NativeString;
+import cpp.RawConstPointer;
 import u3dx.libs_core.cpp.hx_extern.core.Urho_Context;
+import u3dx.libs_core.cpp.hx_extern.graphics.Urho_Texture2D;
+import u3dx.libs_core.cpp.hx_extern.math.Urho_IntRect;
+import u3dx.libs_core.cpp.hx_extern.math.Urho_IntVector2;
+import u3dx.libs_core.cpp.hx_extern.math.Urho_Vector2;
 import u3dx.libs_core.cpp.hx_extern.resource.Urho_Resource;
 @:include("Urho3d/Source/Engine/Urho2D/SpriteSheet2D.h")
 /**
@@ -12,11 +19,10 @@ extern class Urho_SpriteSheet2D extends Urho_Resource
 {
 	@:native("new Urho3D::SpriteSheet2D")
 	public static function create(context:Urho_Context):Urho_SpriteSheet2D;
+	
+	public function GetTexture():Urho_Texture2D;
+	
+	public function GetSprite():Urho_Sprite2D;
+	
+	public function DefineSprite(name:RawConstPointer<Char>,rectangle:Urho_IntRect,hotSpot:Urho_Vector2,offset:Urho_IntVector2):Void;
 }
-
-/*
-RegisterResource<SpriteSheet2D>(engine, "SpriteSheet2D");
-    engine->RegisterObjectMethod("SpriteSheet2D", "Texture2D@+ get_texture() const", asMETHOD(SpriteSheet2D, GetTexture), asCALL_THISCALL);
-    engine->RegisterObjectMethod("SpriteSheet2D", "Sprite2D@+ GetSprite(const String&)", asMETHOD(SpriteSheet2D, GetSprite), asCALL_THISCALL);
-    engine->RegisterObjectMethod("SpriteSheet2D", "void DefineSprite(const String&, const IntRect&, const Vector2& hotSpot=Vector2(0.5f, 0.5f), const IntVector2& offset = IntVector2::ZERO)", asMETHOD(SpriteSheet2D, DefineSprite), asCALL_THISCALL);
-*/
