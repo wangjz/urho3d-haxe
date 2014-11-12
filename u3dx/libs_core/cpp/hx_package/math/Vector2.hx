3b57d650cdb2bc5a3082c06b55a30d8080c39762
wangjz <421924396@qq.com>
2014-11-12 19:10:58 +0800

    Update Vector2.hx

--- u3dx/libs_core/cpp/hx_package/math/Vector2.hx
+++ u3dx/libs_core/cpp/hx_package/math/Vector2.hx
@@ -1,14 +1,16 @@
 package u3dx.libs_core.cpp.hx_package.math;
+import cpp.Pointer;
+import u3dx.libs_core.cpp.hx_extern.math.Urho_Vector2;
 
 /**
  * ...
  * @author ...
  */
-class Vector2
+@:forward abstract Vector2(Pointer<Urho_Vector2>) from Pointer<Urho_Vector2> to Pointer<Urho_Vector2>
 {
 
-	public function new()
+	public inline function new(x:Float,y:Float)
 	{
-		
+		this = Urho_Vector2.createPointer(x, y);
 	}
 }
